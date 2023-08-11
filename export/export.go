3f80c739b9c7ee81c9ce5c7dfc48b6aa7f2dc0f6
Joao Arthur Brunet Monteiro <joaoarthurbm@gmail.com>
2023-08-11 14:49:52 -0300

    deploy

---
+++ export/export.go
@@ -0,0 +1,5 @@
+package export
+
+func GetExport() string {
+    return "export!!!"
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "rsc.io/quote"
+    "fmt"
+)
+
+
+func main() {
+    fmt.Println(quote.Hello())
+}
